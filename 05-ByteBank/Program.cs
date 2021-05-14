1bc6e3c1dc1527b58cbba7b5a1b573fa42a82c89
Jose Machoski <jose.machoski@gmail.com>
2021-05-13 23:50:44 -0300

    Learning OOP CSharp

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cliente gabriela = new Cliente();
+
+
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Desenvolvedora C#";
+            // gabriela.CPF = "434.562.878-20";
+
+            ContaCorrente Conta = new ContaCorrente();
+
+            // Conta.titular = gabriela;
+            Conta.titular = new Cliente();
+            Conta.titular.nome = "Gabriela Costa";
+            Conta.titular.CPF = "434.562.878-20";
+            Conta.titular.profissao = "Desenvolvedora C#";
+
+
+            Conta.saldo = 500;
+            Conta.agencia = 563;
+            Conta.numero = 5634527;
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(Conta.titular.nome);
+            Console.WriteLine(Conta.titular.CPF);
+            Console.WriteLine(Conta.titular.profissao);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434;564.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine("Titular da conta: " + conta.Titular.Nome);
+            Console.WriteLine("Seu saldo é " + "R$" + conta.Saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
